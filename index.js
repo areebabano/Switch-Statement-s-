aa8e23459753b36fb8747a92347452f92c74cecb
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-06-01 19:26:37 +0500

    switch statement's in typescript

---
+++ index.js
@@ -0,0 +1,54 @@
+"use strict";
+// Switch Statement's
+// syntax
+// switch () {
+//     case :
+//         break;
+//     default:
+//         break;
+// }
+// switch with month example
+let month = "January";
+switch (month) {
+    case "January":
+    case "February":
+    case "March":
+        console.log("Finally! Its Fabiha Birthday Month");
+        break;
+    //   case "February":
+    //     console.log("Finally! Its My Birthday Month");
+    //     break;
+    //   case "March":
+    //     console.log("Finally! Its My Birthday Month");
+    //     break;
+    case "April":
+        console.log("This is month of April");
+        break;
+    case "May":
+        console.log("This is month of May");
+        break;
+    case "June":
+        console.log("Finally Its Javeriya Birthday Month");
+        break;
+    case "July":
+        console.log("This is month of July");
+        break;
+    case "August":
+        console.log("This is month of August");
+        break;
+    case "September":
+        console.log("Finally Its Javeriya Nigar Birthday Month");
+        break;
+    case "October":
+        console.log("Finally Its Rubab Birthday Month");
+        break;
+    case "November":
+        console.log("Finally Its My Aniversary Month");
+        break;
+    case "December":
+        console.log("This is month of December");
+        break;
+    default:
+        console.log("Enter the valid month");
+        break;
+}
